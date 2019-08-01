02d2a8f06d5be267309346adce1d6a66da78ed42
Guilherme Coelho <guilherme_megaps@hotmail.com>
2019-08-01 18:36:39 -0300

    Adicionado classe Professor e Alunos; Implementação de Usuários.

---
+++ Bibliotecavirtual/src/usuario/Professor.java
@@ -0,0 +1,17 @@
+package usuario;
+
+public class Professor extends Usuarios {
+
+	@Override
+	public void devolucao() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void emprestimo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- Bibliotecavirtual/src/usuario/Usuarios.java
+++ Bibliotecavirtual/src/usuario/Usuarios.java
@@ -1,5 +1,42 @@
 package usuario;
 
-public class Usuarios {
+public abstract class Usuarios {
+	
+	String nome;
+	String email;
+	String senha;
+	
+	public void setLogin(String email, String senha) {
+		this.email = email;
+		this.senha = senha;
+	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public abstract void devolucao();
+	
+	public abstract void emprestimo();
+	
 }
