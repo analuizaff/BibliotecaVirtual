cf8a19354f99f4da500bf413cebb8464c2536600
Ana Luiza <you@example.com>
2019-08-03 21:33:41 -0300

    remover exemplar

--- Bibliotecavirtual/src/biblioteca/Livros.java
+++ Bibliotecavirtual/src/biblioteca/Livros.java
@@ -1,19 +1,18 @@
-	package biblioteca;
+package biblioteca;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Livros {
 	
-	String code;
 	private String titulo;
 	private String autor;
 	private String edicao;
 	private List <LivroExemplar> exemplar = new ArrayList<LivroExemplar>();
-	private int id =0;
+	private String code; //cada livro tem um codigo
 	private int qtdExemplares = 0;
+	private static int idexemplar =-1; ////todos os exemplares terão um um id de 0 a n. esse id será incrementado toda vez que for criado um novo exemplar, assim o id tbm é a posicao que o livro esta guardado no array
 	
-	private static int contadorId =0;
 	private static ArrayList <Integer> idlivros = new ArrayList <Integer> ();//só guarda os ids dos exeplres, feito apenas para verificações futuras
 	
 	public Livros(String titulo, String autor, String edicao, String code) {
@@ -21,15 +20,20 @@ public class Livros {
 		this.autor = autor;
 		this.edicao = edicao;
 		this.code = code;
-		this.id++;
 	}
-	public void adicionaExemplar(){	//adiciona Exemplar ao meu livro
-		contadorId++;
-		idlivros.add(contadorId);
+	public void adicionaExemplar(){	//adiciona Exemplares ao  livro
+		idexemplar ++;
+		idlivros.add(idexemplar);
 		qtdExemplares++;
-		exemplar.add (new LivroExemplar(titulo, autor, edicao, contadorId));
-		
-		
+		exemplar.add (new LivroExemplar(titulo, autor, edicao, idexemplar));	
+	}
+	
+	public void removeExemplar(int idexemplar) {//remove exemplares do livro pelo id do exemplar
+		qtdExemplares--;
+		idlivros.remove(idexemplar);
+		qtdExemplares--;
+		exemplar.remove(idexemplar);
+		System.out.println("removeu exemplar" );
 	}
 
 	public String getTitulo() {
@@ -48,9 +52,6 @@ public class Livros {
 		this.autor = autor;
 	}
 
-	public int getId() {
-		return id;
-	}
 	
 	public String getEdicao() {
 		return edicao;
@@ -59,23 +60,32 @@ public class Livros {
 	public void setEdicao(String edicao) {
 		this.edicao = edicao;
 	}
+	
+	public int getQtdExemplares() {
+		return qtdExemplares;
+	}
+	public void setQtdExemplares(int qtdExemplares) {
+		this.qtdExemplares = qtdExemplares;
+	}
 
 	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
 	public List<LivroExemplar> getExemplar() {
 		return exemplar;
 	}
 	public void setExemplar(List<LivroExemplar> exemplar) {
 		this.exemplar = exemplar;
 	}
-	public static int getContadorId() {
-		return contadorId;
-	}
-	public static void setContadorId(int contadorId) {
-		Livros.contadorId = contadorId;
-	}
+	
+	
 	
 	@Override
 	public String toString() {
-		return "Livros [" +getTitulo()+  ", autor:" +getAutor()+ ", edicaoo:" +getEdicao()+ ", id: " +getId()+ ",exemplares: " +qtdExemplares+"]" ;
+		return "Livros [" +getTitulo()+  ", autor:" +getAutor()+ ", edição:" +getEdicao()+ ", código: " +getCode()+ ",exemplares: " +qtdExemplares+"]" ;
 	}
 }
